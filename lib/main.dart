0a7f0dca0388dba72b0773df60b9c51d46660ece
Abhay Dutt <dutt.abhay@gmail.com>
2023-10-05 10:39:01 +0530

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDLYRyt7osIeDa10hc2AE6UxD5jN7EZ4AY",
+            appId: "instagram-clone-98d56.firebaseapp.com",
+            messagingSenderId: "130455019505",
+            projectId: "instagram-clone-98d56",
+            storageBucket: "instagram-clone-98d56.appspot.com",
+            ));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -25,4 +40,4 @@ class MyApp extends StatelessWidget {
           webScreenLayout: WebScreenLayout()),
     );
   }
-} 
+}
