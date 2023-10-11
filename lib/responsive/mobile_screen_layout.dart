3079c6ce5828c169372a6b5efb075196c95a5f4e
Abhay Dutt <dutt.abhay@gmail.com>
2023-10-11 18:35:01 +0530

    setup mobile_screen_layout

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,14 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
+
+  @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  String username = "";
+
+//cannot make this async, will give errors
+  @override
+  void initState() {
+    super.initState();
+    getUsername();
+  }
+
+  void getUsername() async {
+    DocumentSnapshot snap = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+    setState(() {
+      username = (snap.data() as Map<String,dynamic>)['username'];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
-        child:Text('This is Mobile'),
-      )
-    );
+        body: Center(
+      child: Text('$username'),
+    ));
   }
 }
